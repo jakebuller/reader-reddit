bdf4988c86ff9f9d66481945bfeca7682b3a5bf1
Jake Buller <jake.buller@enflick.com>
2017-08-24 18:05:31 -0400

    deselect row after click in reddit posts controller and subreddit search controller

--- RedditReader/RedditReader/RedditPostsTableViewController.swift
+++ RedditReader/RedditReader/RedditPostsTableViewController.swift
@@ -215,38 +215,7 @@ class RedditPostsTableViewController: UIViewController, UISearchBarDelegate, UIT
         self.subreddit.loadPosts(completion: self.postsLoaded)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+ }
